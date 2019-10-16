86928b6efd0e201d7c3ce62f937fd4164221ffb9
Maro Balestracci <2109711@talnet.nl>
2019-10-16 09:33:53 +0200

    Uitwerking van de exercise

--- rijbewijs.php
+++ rijbewijs.php
@@ -1,9 +1,10 @@
 <?php
-$line = readline("> ");
-$magniet = 16.49;
+	echo "Hoe oud ben je?" .PHP_EOL;
+$line=readline();
+$magniet < 16.49;
 if ($line > $magniet) {
     echo "Je mag beginnen met rijlessen!";
-} else { 
-echo "Helaas, je mag nog niet beginnen met rijlessen!";
+} 
+else { 
+    echo "Helaas, je mag nog niet beginnen met rijlessen!";
 }
-?>
